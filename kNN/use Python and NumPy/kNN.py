b3dcdb2b831b3496b834bc1df3f4a515d3f17bdf
henry-48 <66840357+henry-48@users.noreply.github.com>
2021-11-23 15:57:22 +0800

    Create kNN.py
    KNN comment update

--- kNN/use Python and NumPy/kNN.py
+++ kNN/use Python and NumPy/kNN.py
@@ -10,17 +10,29 @@ import operator
 from os import listdir
 
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]                  
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet      
+    '''
+    :param  Matrix inX: test case
+    :param Matrix dataSet: known classes that used to compare with the test one.
+    :param List labels: different classes
+    :param Int k: the number of neighbour used to check
+    :return: class
+    '''
+    dataSetSize = dataSet.shape[0]
+    # return the row number if shape[1] return column number
+    diffMat = tile(inX,(dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)                  
+    # array.sum(axis=1)按行累加，axis=0为按列累加
+    sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()            
+    # L2 norm is calculated above
+    sortedDistIndicies = distances.argsort()
+    # argsort() sort the data in increasing order and return the index
     classCount={}                                      
     for i in range(k):
         voteIlabel = labels[sortedDistIndicies[i]]
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    # reverse = True decrease, reverse = False increase
     return sortedClassCount[0][0]
 
 def img2vector(filename):
@@ -54,7 +66,11 @@ def handwritingClassTest():
         classNumStr = int(fileStr.split('_')[0])
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
         if (classifierResult != classNumStr): errorCount += 1.0
-    print "\nthe total number of errors is: %d" % errorCount
-    print "\nthe total error rate is: %f" % (errorCount/float(mTest))
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount/float(mTest)))
+
+
+if __name__ == '__main__':
+    handwritingClassTest()
